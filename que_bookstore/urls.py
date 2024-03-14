1e9d4a9dbc803700f5a0c89a6fb30d0b9ef64b1a
PhucQuach1907 <phucquach1907@gmail.com>
2024-03-14 11:45:07 +0700

    add path default

--- que_bookstore/urls.py
+++ que_bookstore/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('cart/', include('cart.urls')),
+    path('', lambda request: redirect('/book-store/')),
     path('book-store/', include('book.urls')),
     path('mobile-store/', include('mobile.urls')),
     path('search/', include('search.urls')),
