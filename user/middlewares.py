3ec4226b9aeec57bd33e5b88c140356bfc47982a
PhucQuach1907 <phucquach1907@gmail.com>
2024-03-27 22:54:06 +0700

    Force user login before access store

--- user/middlewares.py
+++ user/middlewares.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
 class AddUserToContextMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -6,3 +11,14 @@ class AddUserToContextMiddleware:
         request.user = request.user if request.user.is_authenticated else None
         response = self.get_response(request)
         return response
+
+
+class RedirectToLoginMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not request.user.is_authenticated and request.path != reverse('login'):
+            print('RedirectToLoginMiddleware')
+            return HttpResponseRedirect(reverse('login'))
+        return self.get_response(request)
